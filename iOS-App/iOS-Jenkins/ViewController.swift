373d376a2977460114f8883f7f1b2b1986dc200e
saurabhj70 <saurabh.srivastava@jabong.com>
2018-10-26 12:40:57 +0530

    Initial-coomit

---
+++ iOS-App/iOS-Jenkins/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS-Jenkins
+//
+//  Created by Savrabh  on 26/10/18.
+//  Copyright © 2018 Saurabh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
