f47ead4d8a18fe65b217dc0fd95d45a14aefa8d8
saurabhj70 <saurabh.srivastava@jabong.com>
2018-10-26 17:31:52 +0530

    -update name for test case

--- iOS-App/iOS-JenkinsUITests/iOS_JenkinsUITests.swift
+++ iOS-App/iOS-JenkinsUITests/iOS_JenkinsUITests.swift
@@ -28,7 +28,7 @@ class iOS_JenkinsUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    func testCheckUI() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         
